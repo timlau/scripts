472d04bedfbea28adf300000601096c86d4dfc12
Tim Lauridsen <tla@rasmil.dk>
2019-04-26 18:58:00 +0200

    Add backup scripts

---
+++ backup-sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Backup to google drive
+options="--ignore-existing --progress --transfers 4 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s"
+source="/home/tim/Documents"
+target="gdrive:sync"
+filefrom="--include-from backup-dirs.txt"
+echo "Starting backup to Google drive"
+echo
+rclone sync $options $filefrom $source $target
