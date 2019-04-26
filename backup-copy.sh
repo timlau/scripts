b5d18795e130137ed82451c109126e534f0ec20a
Tim Lauridsen <tla@rasmil.dk>
2019-04-26 20:24:27 +0200

    add logging to backup scripts

--- backup-copy.sh
+++ backup-copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Backup to google drive
-options="--ignore-existing --progress --transfers 4 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s"
+options="--ignore-existing --progress --transfers 4 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 10s --verbose --log-file backup.log"
 source="/home/tim/Documents"
 target="gdrive:sync"
 filefrom="--include-from backup-dirs.txt"
